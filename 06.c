ecfa46774abe11c581cd9e33c61dbbccde01236f
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-12-06 15:13:53 +0100

    Fix data loading and processing in Day 6
    Make the code structure clearer. Unify the data loading process as it is enough.

--- 06.c
+++ 06.c
@@ -5,122 +5,7 @@
 #include <errno.h>
 #include <string.h>
 
-#define ALLOC_BATCH 16
-
-int process_numlist( uint64_t **items, int *items_n, char *line )
-{
-    char *end_p = line;
-    int items_l = 0;
-
-    if ( items_n != NULL )
-        items_l = *items_n;
-
-
-    while ( *end_p != '\n' && strlen( end_p ) != 0 )
-    {
-        if ( items_n != NULL && items_l % ALLOC_BATCH == 0 )
-        {
-            int new_n = items_l + ALLOC_BATCH;
-            uint64_t *tmp = realloc( *items, new_n * sizeof( uint64_t ) );
-            if ( tmp == NULL )
-                return -1;
-            *items = tmp;
-        }
-
-        errno = 0;
-        uint64_t num = strtoull( end_p, &end_p, 10 );
-        if ( errno == EINVAL )
-        {
-            warn( "reading a number" );
-            return -1;
-        }
-        while ( *end_p == ' ' )
-            ++end_p;
-
-        ( *items )[ items_l ] = num;
-        ++items_l;
-    }
-
-    if ( items_n != NULL )
-        *items_n = items_l;
-
-    return 0;
-}
-
-int data_load_one( uint64_t **nums, int *lines, int *line_size, FILE *in_file )
-{
-    int rv = -1;
-    ssize_t bytes_r;
-    size_t n = 0;
-    char *line = NULL;
-    int idx = 0;
-
-    uint64_t *nums_line = NULL;
-    int nums_line_n = 0;
-
-    bytes_r = getdelim( &line, &n, '\n', in_file );
-    if ( ( bytes_r < 0 && errno != 0 ) || line == NULL )
-    {
-        warn( "reading from a file" );
-        goto final;
-    }
-
-    if ( process_numlist( &nums_line, &nums_line_n, line ) < 0 )
-        goto final;
-
-    uint64_t *tmp = realloc( nums_line, ALLOC_BATCH * sizeof( uint64_t ) * nums_line_n );
-    if ( tmp == NULL )
-        goto final;
-    nums_line = tmp;
-
-    errno = 0;
-    while ( ( bytes_r = getdelim( &line, &n, '\n', in_file ) ) > 0 )
-    {
-        ++idx;
-        if ( idx % ALLOC_BATCH == 0 )
-        {
-            int new_n = idx + ALLOC_BATCH;
-            tmp = realloc( nums_line, new_n * sizeof( uint64_t ) * nums_line_n );
-            if ( tmp == NULL )
-                goto final;
-            nums_line = tmp;
-        }
-        if ( *line == '*' || *line == '+' )
-            break;
-
-        uint64_t *actual_nums_line = &nums_line[ idx * nums_line_n ];
-        if ( process_numlist( &actual_nums_line, NULL, line ) < 0 )
-            goto final;
-    }
-
-    if ( bytes_r < 0 && errno != 0 )
-    {
-        warn( "reading from a file" );
-        goto final;
-    }
-
-    char *line_p = line;
-    int op_idx = 0;
-    for ( ; *line_p != '\0'; ++line_p )
-    {
-        if ( *line_p == '*' )
-            nums_line[ ( idx * nums_line_n ) + op_idx++ ] = 1;
-
-        if ( *line_p == '+' )
-            nums_line[ ( idx * nums_line_n ) + op_idx++ ] = 2;
-    }
-
-    rv = 0;
-final:
-    *nums = nums_line;
-    *line_size = nums_line_n;
-    *lines = idx;
-
-    if ( line != NULL )
-        free( line );
-
-    return rv;
-}
+#define ALLOC_BATCH 32
 
 uint64_t add_op( const uint64_t a, const uint64_t b )
 {
@@ -132,41 +17,7 @@ uint64_t mult_op( const uint64_t a, const uint64_t b )
     return a * b;
 }
 
-ssize_t part_one( FILE *in_file )
-{
-    ssize_t rv = -1;
-
-    int lines_n = 0, lines_size = 0;
-    uint64_t *lines = NULL;
-
-    if ( data_load_one( &lines, &lines_n, &lines_size, in_file ) < 0 )
-        goto finalize;
-
-    uint64_t ( *operator )( const uint64_t, const uint64_t );
-    uint64_t num, grand_total = 0;
-
-    for ( int column = 0; column < lines_size; ++column )
-    {
-        operator = lines[ lines_size * lines_n + column ] == 1 ? mult_op : add_op;
-        num = operator == mult_op ? 1 : 0;
-
-        for ( int line_idx = 0; line_idx < lines_n; ++line_idx )
-            num = operator( num, lines[ lines_size * line_idx + column ] );
-
-        grand_total += num;
-    }
-
-    rv = ( ssize_t ) grand_total;
-
-finalize:
-
-    if ( lines != NULL )
-        free( lines );
-
-    return rv;
-}
-
-int data_load_two( char ***data, int *lines, FILE *in_file )
+int data_load( char ***data, int *lines, FILE *in_file )
 {
     int rv = -1;
     ssize_t bytes_r;
@@ -214,16 +65,45 @@ int data_load_two( char ***data, int *lines, FILE *in_file )
     return rv;
 }
 
-ssize_t part_two( FILE *in_file )
+ssize_t part_one( char **lines, int lines_n )
 {
-    ssize_t rv = -1;
+    int greatest_end_idx = 0;
 
-    int lines_n = 0;
-    char **lines = NULL;
+    uint64_t ( *operator )( const uint64_t, const uint64_t );
+    uint64_t num, grand_total = 0;
+    char *end_p;
 
-    if ( data_load_two( &lines, &lines_n, in_file ) < 0 )
-        goto finalize2;
+    while ( lines[ 0 ][ greatest_end_idx ] != '\0' )
+    {
+        operator = lines[ lines_n ][ greatest_end_idx ] == '*' ? mult_op : add_op;
+        num = operator == mult_op ? 1 : 0;
 
+        int dif_end = 0;
+        for ( int line_idx = 0; line_idx < lines_n; ++line_idx )
+        {
+            errno = 0;
+            uint64_t read_num = strtoull( &lines[ line_idx ][ greatest_end_idx ], &end_p, 10 );
+            if ( errno == EINVAL )
+            {
+                warn( "reading a number" );
+                return -1;
+            }
+
+            num = operator( num, read_num );
+
+            if ( end_p - &lines[ line_idx ][ greatest_end_idx ] > dif_end )
+                dif_end = ( int ) ( end_p - &lines[ line_idx ][ greatest_end_idx ] );
+        }
+
+        grand_total += num;
+        greatest_end_idx += dif_end + 1;
+    }
+
+    return ( ssize_t ) grand_total;
+}
+
+ssize_t part_two( char **lines, int lines_n )
+{
     char num_buf[ 20 ] = { 0 };
     uint64_t ( *operator )( const uint64_t, const uint64_t ) = add_op;
     uint64_t num_part = 0, grand_total = 0;
@@ -253,15 +133,28 @@ ssize_t part_two( FILE *in_file )
         if ( errno == EINVAL )
         {
             warn( "reading a number" );
-            goto finalize2;
+            return -1;
         }
 
         num_part = operator( num_part, num );
     }
 
-    rv = ( ssize_t ) grand_total;
+    return ( ssize_t ) grand_total;
+}
 
-finalize2:
+ssize_t process_data( FILE *in_file, ssize_t ( *part_fn )( char**, int ) )
+{
+    ssize_t rv = -1;
+
+    int lines_n = 0;
+    char **lines = NULL;
+
+    if ( data_load( &lines, &lines_n, in_file ) < 0 )
+        goto finalize;
+
+    rv = part_fn( lines, lines_n );
+
+finalize:
     if ( lines != NULL )
     {
         for ( int i = 0; i < lines_n + 1; ++i )
@@ -275,7 +168,7 @@ finalize2:
 
 int main( int argc, char *argv[ ] )
 {
-    ssize_t ( *part_fn )( FILE* ) = part_two;
+    ssize_t ( *part_fn )( char**, int ) = part_one;
     int rv = 0;
     FILE *in_file = stdin;
     size_t sum = 0;
@@ -287,7 +180,7 @@ int main( int argc, char *argv[ ] )
             err( 1, "opening file" );
     }
 
-    if ( ( sum = part_fn( in_file ) ) < 0 )
+    if ( ( sum = process_data( in_file, part_fn ) ) < 0 )
         rv = 1;
 
     if ( in_file != stdin )
